0d9723e48f4690439732e2746d62705b05066305
Muremi Peter <petermuremi351@gmail.com>
2026-01-24 14:25:33 +0300

    create ulrs of cart app

--- MarlyCakesPastries/urls.py
+++ MarlyCakesPastries/urls.py
@@ -1,10 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('store.urls')),
-    path('cart/', include('cart.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.cart_summary, name="cart_summary"),
+    path('add/', views.cart_add, name="cart_add"),
+    path('delete/', views.cart_delete, name="cart_delete"),
+    path('update/', views.cart_update, name="cart_update"),
+
+    
+]
