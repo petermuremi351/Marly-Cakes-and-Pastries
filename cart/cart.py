66dd5433213728406053c94868ee4ef9daf62268
Muremi Peter <petermuremi351@gmail.com>
2026-01-24 16:37:35 +0300

    cart.py, context processor reg in settings

---
+++ cart/cart.py
@@ -0,0 +1,14 @@
+
+class Cart():
+    def __init__(self, request):
+        
+        # get the current session key if it exists
+        cart = self.session.get('session_key')
+
+        # if the user is new, no session key. create one
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+
+        # make sure cart is available in all pages
+        self.cart = cart
+            
