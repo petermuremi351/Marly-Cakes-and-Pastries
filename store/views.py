81611dcf1bafc0afa7d54a7ee9c1a65cece66b59
Muremi Peter <petermuremi351@gmail.com>
2026-01-24 14:04:38 +0300

    categories page

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -8,6 +8,19 @@ from .forms import SignUpForm
 from django import forms
 
 
+def category(request, foo):
+    # Replace hyphens with spaces
+    foo = foo.replace('-', ' ')
+    # grab category from url
+    try:
+        # look up the category
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+
+    except:
+        messages.success(request, ("That Category Doesn't Exist..."))
+        return redirect('home')
 def product(request,pk):
     product = Product.objects.get(id=pk)
     return render(request, 'product.html', {'product':product})
