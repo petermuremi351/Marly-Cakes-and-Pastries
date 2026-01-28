4e2d8d7b5f2a28d8db1decedddc1b5f847b8a9fc
Muremi Peter <petermuremi351@gmail.com>
2026-01-28 22:09:15 +0300

    add up cart totals

--- cart/cart.py
+++ cart/cart.py
@@ -29,6 +29,27 @@ class Cart:
 
         self.session.modified = True
 
+    def cart_total(self):
+        #    get product ids
+        product_ids = self.cart.keys()
+        # lookup the keys in prod db model
+        products = Product.objects.filter(id__in=product_ids)
+        # get quantities
+        quantities = self.cart
+        # start counting at 0
+        total = 0
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else: 
+                        total = total + (product.price * value)   
+        return total            
+
+        
+
     def __len__(self):
         return len(self.cart)
 
