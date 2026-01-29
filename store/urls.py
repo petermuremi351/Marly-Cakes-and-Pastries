8cd229d785d1a8633d6200d979b6348d95bb4961
Muremi Peter <petermuremi351@gmail.com>
2026-01-29 23:54:31 +0300

    update user profile

--- store/urls.py
+++ store/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
    path('product/<int:pk>', views.product, name='product'),
    path('category/<str:foo>', views.category, name='category'),
    path('category_summary/', views.category_summary, name='category_summary'),
+   path('update_user/', views.update_user, name='update_user'),
 ]
