5f97082de1d7fe9bcc15f857639e0367b7895e99
Muremi Peter <petermuremi351@gmail.com>
2026-02-26 11:28:59 +0300

    shipping information

--- store/views.py
+++ store/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
 from django import forms
 from django.db.models import Q
 import json
@@ -13,16 +15,34 @@ from cart.cart import Cart
 
 def update_info(request):
     if request.user.is_authenticated:
+        # get current user
         current_user = Profile.objects.get(user__id=request.user.id)
+        # get current users shipping info
+        shipping_user = ShippingAddress.objects.get(usr__id=request.user.id)
+        # get original user info
         form = UserInfoForm(request.POST or None, instance=current_user)
+        # get user shipping info
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
 
-        if form.is_valid():
-            form.save
+        # if form.is_valid(): or shipping_form.is_valid():
+        #     form.save()
+        #     shipping_form.save()
 
             
+        #     messages.success(request, "Your Info has Been Updated!")
+        #     return redirect("home")
+        # return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
+        if form.is_valid() or shipping_form.is_valid():
+            # save original form
+            form.save()
+            # save shipping form
+            shipping_form.save()
+
             messages.success(request, "Your Info has Been Updated!")
             return redirect("home")
-        return render(request, "update_info.html", {'form':form})
+        return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
+    
+
     else:
         messages.success(request, "You Must Be Logged In To Access That Page!!")
         return redirect("home")
