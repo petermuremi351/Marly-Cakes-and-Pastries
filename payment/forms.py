77cf01f3b02904a985ee84c92b57c501764b3d3b
Muremi Peter <petermuremi351@gmail.com>
2026-02-27 22:10:48 +0300

    billing information

--- payment/forms.py
+++ payment/forms.py
@@ -18,3 +18,16 @@ class ShippingForm(forms.ModelForm):
 
         exclude = ['user',]
 
+
+class PaymentForm(forms.Form):
+    card_name = shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Card name'}), required=True)
+    card_number = shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Number on Card'}), required=True)
+    card_exp_date = shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Card Expiry date'}), required=True)
+    card_cvv_number =shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'cvv code'}), required=True)
+    card_address1 = shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing address1'}), required=True)
+    card_address2 =shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing address2'}), required=False)
+    card_city =shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing City'}), required=True)
+    card_state =shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing state'}), required=True)
+    card_zipcode = shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing zipcode'}), required=True)
+    card_country =shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing Country'}), required=True)
+   
--- payment/views.py
+++ payment/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from payment.forms import ShippingForm
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress
+from django.contrib import messages
 
 # Create your views here.
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # check to see if user is logged in
+        if request.user.is_authenticated:
+            # get the billing form
+            billing_form = PaymentForm()
+            return render(request, "billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
+        else:
+            # not logged in
+            # get the billing form
+            billing_form = PaymentForm()
+            return render(request, "billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
+            
+            pass
+            
+
+
+        shipping_form = request.POST
+        return render(request, "billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form })
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
+
 def payment_success(request):
     return render(request, "payment_success.html", {})
 
